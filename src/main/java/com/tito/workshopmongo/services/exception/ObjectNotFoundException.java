b62aa6a0a732e57124dc6217eccd43cacdde6905
francitonioferreiraalves <francitonio@gmail.com>
2022-09-28 15:58:54 -0300

    criação Domain, Dto, services, etc

---
+++ src/main/java/com/tito/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.tito.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
